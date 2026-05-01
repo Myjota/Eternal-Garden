21f42e911ae956e070030fdd9faa9487f811ee39
Myjota <163742208+Myjota@users.noreply.github.com>
2026-05-01 12:26:57 +0300

    Delete components/memorial/

--- components/memorial/blocks/index.ts
+++
@@ -1,4 +0,0 @@
-export { BiographyBlock } from './BiographyBlock'
-export { CandleWall } from './CandleWall'
-export { FamilyBlock } from './FamilyBlock'
-export { SupportBlock } from './SupportBlock'
