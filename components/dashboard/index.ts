ce7be800c5f157072973c33667d525cf84a5b9ea
Myjota <163742208+Myjota@users.noreply.github.com>
2026-05-01 12:30:53 +0300

    Delete components/

--- components/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export { DashboardSidebar } from './DashboardSidebar'
-export { MemorialCard } from './MemorialCard'
-export { ThemeCard } from './ThemeCard'
-export { UpgradeCard } from './UpgradeCard'
