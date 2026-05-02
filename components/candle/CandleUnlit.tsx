4bf1aec56721462452534cc214507e10537a83b7
Myjota <163742208+Myjota@users.noreply.github.com>
2026-05-02 14:50:43 +0300

    Update CandleUnlit.tsx

--- components/candle/CandleUnlit.tsx
+++ components/candle/CandleUnlit.tsx
@@ -5,13 +5,21 @@ import "./candle.css";
 export function CandleUnlit() {
   return (
     <div className="candle-wrapper">
-      <div className="candle unlit">
+      
+      {/* 🧱 SAME SCALE AS LIT */}
+      <div className="candle unlit candle-small">
+
+        {/* WAX */}
         <div className="wax">
           <div className="wax-top cold" />
         </div>
 
+        {/* WICK */}
         <div className="wick cold" />
+
+        {/* GLOW (kept for consistency, but subtle in CSS) */}
         <div className="glow" />
+
       </div>
     </div>
   );
